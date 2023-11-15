860e755c49b40debb9888f5049337e72fd4a0470
ELophem <18072@ecam.be>
2023-11-15 16:37:01 +0100

    Upload image

--- frontend-ia/src/Upload/Upload.js
+++ frontend-ia/src/Upload/Upload.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UploadPage = () => {
-  // You can define functions or variables here
+  const [file, setFile] = useState();
+
+  const handleChange = (e) => {
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  };
 
   return (
     <div>
-      <h1>Welcome to upload</h1>
+      <h2>Add Image:</h2>
+      <input type="file" onChange={handleChange} />
+      {file && (
+        <div style={{ maxWidth: '400px' }}> {/* Adjust the maximum width as needed */}
+          <img src={file} alt="Uploaded file" style={{ maxWidth: '100%', height: 'auto' }} />
+        </div>
+      )}
       <p>This is a simple example of a React page.</p>
     </div>
   );
