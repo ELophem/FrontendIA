398e1252d688b493ff241aaf2225551d5c15b1d4
Pedropinto29 <pedrodacostapinto@gmail.com>
2023-12-13 14:53:40 +0100

    Link to api working

--- frontend-ia/src/Gallery/Gallery.js
+++ frontend-ia/src/Gallery/Gallery.js
@@ -1,102 +1,245 @@
+// import React, { useEffect, useRef, useState } from 'react';
+// import { useLocation } from 'react-router-dom';
+// import { storage } from '../Firebase/firebaseConfig'; 
+// import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'; 
+// import { v4 } from 'uuid'; 
+// import { firestore } from '../Firebase/firebaseConfig';
+// import { addDoc, collection } from 'firebase/firestore';
+
+// const GalleryPage = () => {
+//   const location = useLocation();
+//   const params = new URLSearchParams(location.search);
+//   const file = params.get('file'); 
+
+//   const canvasRef = useRef(null);
+//   const [imageDisplayed, setImageDisplayed] = useState(false); 
+//   const [imageUploaded, setImageUploaded] = useState(false); 
+
+//   useEffect(() => {
+//     if (file && !imageDisplayed) {
+//       const canvas = canvasRef.current;
+//       const ctx = canvas.getContext('2d');
+
+//       const image = new Image();
+//       image.src = file;
+
+//       image.onload = () => {
+//         canvas.width = image.width;
+//         canvas.height = image.height;
+
+//         // Rotate the image by 180°
+//         ctx.translate(image.width / 2, image.height / 2);
+//         ctx.rotate(Math.PI);
+//         ctx.drawImage(image, -image.width / 2, -image.height / 2);
+
+//         const rotatedImage = canvas.toDataURL('image/jpeg');
+
+//         // Display rotated image
+//         const rotatedImageElement = document.createElement('img');
+//         rotatedImageElement.src = rotatedImage;
+
+//         const galleryContainer = document.getElementById('gallery-container');
+//         galleryContainer.innerHTML = '';
+//         galleryContainer.appendChild(rotatedImageElement);
+
+//         setImageDisplayed(true); // Set imageDisplayed to true after image is displayed
+
+//         // Implement AI function here (for future use)
+//         // YourAIFunction(rotatedImage);
+//         // This is where you'll integrate AI processing with the rotated image
+//         // For now, this section serves as a placeholder for AI integration.
+//       };
+//     }
+//   }, [file, imageDisplayed]);
+
+//   useEffect(() => {
+//     if (imageDisplayed && !imageUploaded) {
+//       uploadImage();
+//     }
+//   }, [imageDisplayed, imageUploaded]);
+
+//   const uploadImage = async () => {
+//     try {
+//       const canvas = canvasRef.current;
+//       const rotatedImage = canvas.toDataURL('image/jpeg');
+  
+//       const imageRef = ref(storage, `${v4()}.jpg`);
+//       const byteCharacters = atob(rotatedImage.split(',')[1]);
+//       const byteNumbers = new Array(byteCharacters.length);
+  
+//       for (let i = 0; i < byteCharacters.length; i++) {
+//         byteNumbers[i] = byteCharacters.charCodeAt(i);
+//       }
+  
+//       const byteArray = new Uint8Array(byteNumbers);
+//       const imageBlob = new Blob([byteArray], { type: 'image/jpeg' });
+  
+//       const snapshot = await uploadBytes(imageRef, imageBlob);
+//       console.log('Image uploaded successfully:', snapshot);
+  
+//       // Get download URL of the uploaded image
+//       const downloadURL = await getDownloadURL(snapshot.ref);
+//       console.log('Download URL:', downloadURL);
+  
+//       const db = firestore;
+//       console.log(db);
+  
+//       const personsRef = collection(db, 'Persons');
+//       await addDoc(personsRef, {
+//         name: 'John',
+//         surname: 'Doe',
+//         photo: downloadURL // Store the download URL in the Firestore document
+//       });
+  
+//       setImageUploaded(true); // Set imageUploaded to true after successful upload
+//     } catch (error) {
+//       console.error('Error uploading image:', error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h2>Gallery:</h2>
+//       <div id="gallery-container">
+//         <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default GalleryPage;
+
 import React, { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { storage } from '../Firebase/firebaseConfig'; 
-import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'; 
-import { v4 } from 'uuid'; 
+import { storage } from '../Firebase/firebaseConfig';
+import { ref, uploadBytes } from 'firebase/storage';
+import { v4 } from 'uuid';
+import { getDownloadURL} from 'firebase/storage'; 
 import { firestore } from '../Firebase/firebaseConfig';
 import { addDoc, collection } from 'firebase/firestore';
-
+import axios from 'axios';
+ 
 const GalleryPage = () => {
   const location = useLocation();
   const params = new URLSearchParams(location.search);
-  const file = params.get('file'); 
-
+  const file = params.get('file');
+ 
   const canvasRef = useRef(null);
-  const [imageDisplayed, setImageDisplayed] = useState(false); 
-  const [imageUploaded, setImageUploaded] = useState(false); 
-
+  const [imageDisplayed, setImageDisplayed] = useState(false);
+  const [imageUploaded, setImageUploaded] = useState(false);
+   
+  useEffect(() => {
+ 
+    const sendImageToBackend = async (imageDataURL) => {
+ 
+      try {
+ 
+        const response = await axios.post('http://127.0.0.1:5000/recognize', { image: imageDataURL });
+        console.log('Response from backend:', response.data);
+        // Further actions based on backend response
+        setImageDisplayed(true); // Set imageDisplayed to true after image is "sent" to backend
+      } catch (error) {
+        // Handle errors, if any, during the API call
+        console.error('Error sending image to backend:', error);
+      }
+ 
+    };
+   
+    const loadImageAndSendToBackend = async () => {
+      if (file && !imageDisplayed) {
+        const image = new Image();
+        image.onload = () => {
+          const canvas = document.createElement('canvas');
+          const ctx = canvas.getContext('2d');
+          canvas.width = image.width;
+          canvas.height = image.height;
+          ctx.drawImage(image, 0, 0);
+          const imageDataURL = canvas.toDataURL('image/jpeg');
+          sendImageToBackend(imageDataURL);
+        };
+        image.src = file;
+      }
+ 
+    };
+   
+    loadImageAndSendToBackend(); // Call the function to load image and send to backend
+   
+  }, [file, imageDisplayed]);
+ 
+ 
   useEffect(() => {
     if (file && !imageDisplayed) {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext('2d');
-
+ 
       const image = new Image();
       image.src = file;
-
-      image.onload = () => {
+ 
+      image.onload = () =>  {
+ 
         canvas.width = image.width;
         canvas.height = image.height;
-
-        // Rotate the image by 180°
-        ctx.translate(image.width / 2, image.height / 2);
-        ctx.rotate(Math.PI);
-        ctx.drawImage(image, -image.width / 2, -image.height / 2);
-
-        const rotatedImage = canvas.toDataURL('image/jpeg');
-
-        // Display rotated image
-        const rotatedImageElement = document.createElement('img');
-        rotatedImageElement.src = rotatedImage;
-
-        const galleryContainer = document.getElementById('gallery-container');
-        galleryContainer.innerHTML = '';
-        galleryContainer.appendChild(rotatedImageElement);
-
-        setImageDisplayed(true); // Set imageDisplayed to true after image is displayed
-
-        // Implement AI function here (for future use)
-        // YourAIFunction(rotatedImage);
-        // This is where you'll integrate AI processing with the rotated image
-        // For now, this section serves as a placeholder for AI integration.
+        ctx.drawImage(image, -image.width / 2, -image.height / 2)
+ 
+        const ImageIA = canvas.toDataURL('image/jpg');
+       
+        axios.post('http://127.0.0.1:5000/recognize', ImageIA)
+          .then(response => {
+            console.log('API response:', response.data);
+            setImageUploaded(true);
+          })
+          .catch(error => {
+            console.error('API error:', error);
+          });
       };
     }
-  }, [file, imageDisplayed]);
-
+}, [file, imageDisplayed]);
+ 
   useEffect(() => {
     if (imageDisplayed && !imageUploaded) {
       uploadImage();
     }
   }, [imageDisplayed, imageUploaded]);
-
+ 
   const uploadImage = async () => {
     try {
       const canvas = canvasRef.current;
       const rotatedImage = canvas.toDataURL('image/jpeg');
-  
+ 
       const imageRef = ref(storage, `${v4()}.jpg`);
       const byteCharacters = atob(rotatedImage.split(',')[1]);
       const byteNumbers = new Array(byteCharacters.length);
-  
+ 
       for (let i = 0; i < byteCharacters.length; i++) {
         byteNumbers[i] = byteCharacters.charCodeAt(i);
       }
-  
+ 
       const byteArray = new Uint8Array(byteNumbers);
       const imageBlob = new Blob([byteArray], { type: 'image/jpeg' });
-  
+ 
       const snapshot = await uploadBytes(imageRef, imageBlob);
       console.log('Image uploaded successfully:', snapshot);
-  
+ 
       // Get download URL of the uploaded image
       const downloadURL = await getDownloadURL(snapshot.ref);
       console.log('Download URL:', downloadURL);
-  
+ 
       const db = firestore;
       console.log(db);
-  
+ 
       const personsRef = collection(db, 'Persons');
       await addDoc(personsRef, {
         name: 'John',
         surname: 'Doe',
         photo: downloadURL // Store the download URL in the Firestore document
       });
-  
+ 
       setImageUploaded(true); // Set imageUploaded to true after successful upload
     } catch (error) {
       console.error('Error uploading image:', error);
     }
   };
-
+ 
   return (
     <div>
       <h2>Gallery:</h2>
@@ -106,5 +249,5 @@ const GalleryPage = () => {
     </div>
   );
 };
-
+ 
 export default GalleryPage;
--- frontend-ia/src/Upload/Upload.js
+++ frontend-ia/src/Upload/Upload.js
@@ -1,30 +1,78 @@
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+
+// const UploadPage = () => {
+//   const [file, setFile] = useState();
+
+//   const handleChange = (e) => {
+//     const uploadedFile = e.target.files[0];
+//     setFile(URL.createObjectURL(uploadedFile));
+//   };
+
+//   return (
+//     <div>
+//       <h2>Add Image:</h2>
+//       <input type="file" onChange={handleChange} />
+//       {file && (
+//         <div style={{ maxWidth: '400px' }}>
+//           <img src={file} alt="Uploaded file" style={{ maxWidth: '100%', height: 'auto' }} />
+//         </div>
+//       )}
+//       <Link to={`/Gallery?file=${encodeURIComponent(file)}`}>
+//         <button>Go to Gallery</button>
+//       </Link>
+//       <p>This is a simple example of a React page.</p>
+//     </div>
+//   );
+// };
+
+// export default UploadPage;
+
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 const UploadPage = () => {
   const [file, setFile] = useState();
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const uploadedFile = e.target.files[0];
-    setFile(URL.createObjectURL(uploadedFile));
+    setFile(uploadedFile);
+  };
+
+  const handleGoToGallery = async () => {
+    if (file) {
+      try {
+        const formData = new FormData();
+        formData.append('image', file);
+
+        const response = await axios.post('http://127.0.0.1:5000/recognize', formData);
+        console.log('Response from backend:', response.data);
+
+        // Redirect to GalleryPage after sending the image to the backend
+        navigate(`/Gallery?file=${encodeURIComponent(file)}`);
+      } catch (error) {
+        console.error('Error sending image to backend:', error);
+      }
+    } else {
+      // Handle case where no file is selected
+      console.error('Please select a file.');
+    }
   };
 
   return (
     <div>
       <h2>Add Image:</h2>
       <input type="file" onChange={handleChange} />
-      {file && (
-        <div style={{ maxWidth: '400px' }}>
-          <img src={file} alt="Uploaded file" style={{ maxWidth: '100%', height: 'auto' }} />
-        </div>
-      )}
-      <Link to={`/Gallery?file=${encodeURIComponent(file)}`}>
-        <button>Go to Gallery</button>
-      </Link>
+      <button onClick={handleGoToGallery}>Go to Gallery</button>
       <p>This is a simple example of a React page.</p>
     </div>
   );
 };
 
 export default UploadPage;
+
 
