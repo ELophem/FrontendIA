57928bac65fc54e42e9a4c18be518acfbc4a2adc
Pedropinto29 <pedrodacostapinto@gmail.com>
2023-12-14 14:23:43 +0100

    Remove comented code

--- frontend-ia/src/Upload/Upload.js
+++ frontend-ia/src/Upload/Upload.js
@@ -1,33 +1,3 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-
-// const UploadPage = () => {
-//   const [file, setFile] = useState();
-
-//   const handleChange = (e) => {
-//     const uploadedFile = e.target.files[0];
-//     setFile(URL.createObjectURL(uploadedFile));
-//   };
-
-//   return (
-//     <div>
-//       <h2>Add Image:</h2>
-//       <input type="file" onChange={handleChange} />
-//       {file && (
-//         <div style={{ maxWidth: '400px' }}>
-//           <img src={file} alt="Uploaded file" style={{ maxWidth: '100%', height: 'auto' }} />
-//         </div>
-//       )}
-//       <Link to={`/Gallery?file=${encodeURIComponent(file)}`}>
-//         <button>Go to Gallery</button>
-//       </Link>
-//       <p>This is a simple example of a React page.</p>
-//     </div>
-//   );
-// };
-
-// export default UploadPage;
-
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
